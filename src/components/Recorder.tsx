a920433b53c88c693581695433303c9424daf85e
prathamskk <prathamskk@gmail.com>
2023-03-26 22:49:42 +0530

    attempt submission UI half done

--- src/components/Recorder.tsx
+++ src/components/Recorder.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useAudioRecorder } from 'react-audio-voice-recorder'
-
+import MicIcon from '@mui/icons-material/Mic'
+import { Stack, Box } from '@mui/material'
 const Recorder = (props: {
   setRecording: React.Dispatch<React.SetStateAction<Blob | undefined>>
 }) => {
@@ -14,18 +15,43 @@ const Recorder = (props: {
 
   return (
     <>
-      <p>{recordingTime}</p>
-      <input
-        type='button'
-        onClick={() => {
-          if (!isRecording) {
-            startRecording()
-          } else {
-            stopRecording()
-          }
-        }}
-        value='Recorder'
-      />
+      <Stack direction='column' justifyContent='center' alignItems='center'>
+        <p>{recordingTime}</p>
+        <Box
+          onClick={() => {
+            if (!isRecording) {
+              startRecording()
+            } else {
+              stopRecording()
+            }
+          }}
+        >
+          <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+            <Box
+              bgcolor='white'
+              border='solid'
+              borderColor='#7852e3'
+              width={150}
+              height={150}
+              borderRadius={'100%'}
+            />
+            <Box
+              sx={{
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+                position: 'absolute',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <MicIcon fontSize='large' htmlColor='#7852e3' />
+            </Box>
+          </Box>
+        </Box>
+      </Stack>
     </>
   )
 }
--- src/pages/scenarios/NewAttemptPage.tsx
+++ src/pages/scenarios/NewAttemptPage.tsx
@@ -1,6 +1,6 @@
 import { doc, serverTimestamp, setDoc } from 'firebase/firestore'
 import { uploadBytes, getDownloadURL } from 'firebase/storage'
-import { useState } from 'react'
+import { useState, FormEvent } from 'react'
 import { useParams } from 'react-router-dom'
 import { useUser } from '../../components/AuthRoute'
 import { Attempt, createSubCollection } from '../../models/collections'
@@ -8,6 +8,12 @@ import { useForm } from 'react-hook-form'
 import Recorder from '../../components/Recorder'
 import { storage } from '../../config/firebase'
 import { ref } from '@firebase/storage'
+import { Button, Checkbox, Paper, Stack, Typography } from '@mui/material'
+import { useMultistepForm } from '../../hooks/useMultistepForm'
+import Box from '@mui/material/Box'
+
+import selfAssessmentQuestions from '../../models/selfAssessmentQuestions.json'
+import selfReflectiveQuestions from '../../models/selfReflectiveQuestions.json'
 
 const NewAttemptPage = () => {
   const { scenarioId } = useParams()
@@ -16,10 +22,26 @@ const NewAttemptPage = () => {
   const [afterRecording, setAfterRecording] = useState<Blob>()
   const { register, handleSubmit } = useForm<Attempt>()
 
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } = useMultistepForm([
+    'BeforeAssessment',
+    'SelfAssessment',
+    'SelfReflective',
+    'AfterAssessment',
+  ])
+
+  function onSubmitForm(e: FormEvent) {
+    e.preventDefault()
+    if (!isLastStep) return next()
+    alert('Successful Account Creation')
+    onSubmit()
+  }
   const onSubmit = handleSubmit(async (data) => {
     if (scenarioId) {
+      console.log('borefoe recording', beforeRecording)
       if (beforeRecording) {
         if (afterRecording) {
+          console.log('before after scenario are defined')
+
           data.scenarioId = scenarioId
           data.hasGraded = false
           data.submissionTimestamp = serverTimestamp()
@@ -47,46 +69,143 @@ const NewAttemptPage = () => {
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <div>
-          <label>Scenario Video</label>
-        </div>
-        <div>
-          <label>Before Assessment Recording</label>
-          <Recorder setRecording={setBeforeRecording} />
-        </div>
-        <div>
-          <label>self assessment questions</label>
-          <input {...register('selfAssessmentAnswers.question1')} type='checkbox' />
-          <input {...register('selfAssessmentAnswers.question2')} type='checkbox' />
-          <input {...register('selfAssessmentAnswers.question3')} type='checkbox' />
-          <input {...register('selfAssessmentAnswers.question4')} type='checkbox' />
-          <input {...register('selfAssessmentAnswers.question5')} type='checkbox' />
-          <input {...register('selfAssessmentAnswers.question6')} type='checkbox' />
-          <input {...register('selfAssessmentAnswers.question7')} type='checkbox' />
-          <input {...register('selfAssessmentAnswers.question8')} type='checkbox' />
-          <input {...register('selfAssessmentAnswers.question9')} type='checkbox' />
-          <input {...register('selfAssessmentAnswers.question10')} type='checkbox' />
-          <input {...register('selfAssessmentAnswers.question11')} type='checkbox' />
-          <input {...register('selfAssessmentAnswers.question12')} type='checkbox' />
-          <input {...register('selfAssessmentAnswers.question13')} type='checkbox' />
-          <input {...register('selfAssessmentAnswers.question14')} type='checkbox' />
-          <input {...register('selfAssessmentAnswers.question15')} type='checkbox' />
-        </div>
-        <div>
-          <label>self reflective questions</label>
-          <input {...register('selfReflectiveAnswers.question1')} type='checkbox' />
-          <input {...register('selfReflectiveAnswers.question2')} type='checkbox' />
-          <input {...register('selfReflectiveAnswers.question3')} type='checkbox' />
-          <input {...register('selfReflectiveAnswers.question4')} type='checkbox' />
-          <input {...register('selfReflectiveAnswers.question5')} type='checkbox' />
-          <input {...register('selfReflectiveAnswers.question6')} type='text' />
-        </div>
-        <div>
-          <label>after assessment recording</label>
-          <Recorder setRecording={setAfterRecording} />
-        </div>
-        <input type='submit' value='submit' />
+      <form onSubmit={onSubmitForm}>
+        <div>{step}</div>
+        <Box display={currentStepIndex == 0 ? 'block' : 'none'}>
+          <Stack direction='column' justifyContent='center' alignItems='center' spacing={2}>
+            <Recorder setRecording={setBeforeRecording} />
+            <Typography variant='h5' align='center'>
+              Record SBAR Briefing
+            </Typography>
+          </Stack>
+        </Box>
+        <Box display={currentStepIndex == 1 ? 'block' : 'none'}>
+          <div>
+            <label>self assessment questions</label>
+            <input {...register('selfAssessmentAnswers.question1')} type='checkbox' />
+            <input {...register('selfAssessmentAnswers.question2')} type='checkbox' />
+            <input {...register('selfAssessmentAnswers.question3')} type='checkbox' />
+            <input {...register('selfAssessmentAnswers.question4')} type='checkbox' />
+            <input {...register('selfAssessmentAnswers.question5')} type='checkbox' />
+            <input {...register('selfAssessmentAnswers.question6')} type='checkbox' />
+            <input {...register('selfAssessmentAnswers.question7')} type='checkbox' />
+            <input {...register('selfAssessmentAnswers.question8')} type='checkbox' />
+            <input {...register('selfAssessmentAnswers.question9')} type='checkbox' />
+            <input {...register('selfAssessmentAnswers.question10')} type='checkbox' />
+            <input {...register('selfAssessmentAnswers.question11')} type='checkbox' />
+            <input {...register('selfAssessmentAnswers.question12')} type='checkbox' />
+            <input {...register('selfAssessmentAnswers.question13')} type='checkbox' />
+            <input {...register('selfAssessmentAnswers.question14')} type='checkbox' />
+            <input {...register('selfAssessmentAnswers.question15')} type='checkbox' />
+          </div>
+          <Typography variant='h6'>Identify</Typography>
+          <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox {...register('selfAssessmentAnswers.question1')} />
+            <Typography>{selfAssessmentQuestions.question1}</Typography>
+          </Stack>
+          {/* <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox checked={attempt.selfAssessmentAnswers.question2} />
+            <Typography>{selfAssessmentQuestions.question2}</Typography>
+          </Stack>
+          <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox checked={attempt.selfAssessmentAnswers.question3} />
+            <Typography>{selfAssessmentQuestions.question3}</Typography>
+          </Stack>
+          <Typography variant='h6'>Situation</Typography>
+
+          <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox checked={attempt.selfAssessmentAnswers.question4} />
+            <Typography>{selfAssessmentQuestions.question4}</Typography>
+          </Stack>
+          <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox checked={attempt.selfAssessmentAnswers.question5} />
+            <Typography>{selfAssessmentQuestions.question5}</Typography>
+          </Stack>
+          <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox checked={attempt.selfAssessmentAnswers.question6} />
+            <Typography>{selfAssessmentQuestions.question6}</Typography>
+          </Stack>
+          <Typography variant='h6'>Background & Assessment</Typography>
+
+          <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox checked={attempt.selfAssessmentAnswers.question7} />
+            <Typography>{selfAssessmentQuestions.question7}</Typography>
+          </Stack>
+          <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox checked={attempt.selfAssessmentAnswers.question8} />
+            <Typography>{selfAssessmentQuestions.question8}</Typography>
+          </Stack>
+          <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox checked={attempt.selfAssessmentAnswers.question9} />
+            <Typography>{selfAssessmentQuestions.question9}</Typography>
+          </Stack>
+          <Typography variant='h6'>Recommendation & Repeat</Typography>
+
+          <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox checked={attempt.selfAssessmentAnswers.question10} />
+            <Typography>{selfAssessmentQuestions.question10}</Typography>
+          </Stack>
+          <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox checked={attempt.selfAssessmentAnswers.question11} />
+            <Typography>{selfAssessmentQuestions.question11}</Typography>
+          </Stack>
+          <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox checked={attempt.selfAssessmentAnswers.question12} />
+            <Typography>{selfAssessmentQuestions.question12}</Typography>
+          </Stack>
+          <Typography variant='h6'>Order & Accuracy</Typography>
+
+          <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox checked={attempt.selfAssessmentAnswers.question13} />
+            <Typography>{selfAssessmentQuestions.question13}</Typography>
+          </Stack>
+          <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox checked={attempt.selfAssessmentAnswers.question14} />
+            <Typography>{selfAssessmentQuestions.question14}</Typography>
+          </Stack>
+          <Stack direction='row' justifyContent='flex-start' alignItems='center'>
+            <Checkbox checked={attempt.selfAssessmentAnswers.question15} />
+            <Typography>{selfAssessmentQuestions.question15}</Typography>
+          </Stack> */}
+        </Box>
+        <Box display={currentStepIndex == 2 ? 'block' : 'none'}>
+          <div>
+            <label>self reflective questions</label>
+            <input {...register('selfReflectiveAnswers.question1')} type='checkbox' />
+            <input {...register('selfReflectiveAnswers.question2')} type='checkbox' />
+            <input {...register('selfReflectiveAnswers.question3')} type='checkbox' />
+            <input {...register('selfReflectiveAnswers.question4')} type='checkbox' />
+            <input {...register('selfReflectiveAnswers.question5')} type='checkbox' />
+            <input {...register('selfReflectiveAnswers.question6')} type='text' />
+          </div>
+        </Box>
+        <Box display={currentStepIndex == 3 ? 'block' : 'none'}>
+          <Stack direction='column' justifyContent='center' alignItems='center' spacing={2}>
+            <Recorder setRecording={setAfterRecording} />
+            <Typography variant='h5' align='center'>
+              Record SBAR Briefing for the second time with the improvements
+            </Typography>
+          </Stack>
+        </Box>
+        {currentStepIndex + 1} / {steps.length}
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+          <Stack direction={'row'} m={1} gap={1}>
+            {!isFirstStep && (
+              <Button size='large' fullWidth={true} variant='contained' onClick={back}>
+                Back
+              </Button>
+            )}
+            <Button
+              color='secondary'
+              size='large'
+              fullWidth={true}
+              variant='contained'
+              type='submit'
+            >
+              {isLastStep ? 'Finish' : 'Next'}
+            </Button>
+          </Stack>
+        </Paper>
       </form>
     </div>
   )
